544679a4a54e0dad7ef270afd143c719fac327ef
inujung <inujung@momenti.tv>
2024-04-10 16:26:09 +0900

    feat: toc

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,95 +1,112 @@
 import { getBlogPosts } from "app/blog/utils";
-import { CustomMDX } from "app/components/mdx";
+import { CustomMDX, parseToc } from "app/components/mdx";
 import { baseUrl } from "app/sitemap";
 import { notFound } from "next/navigation";
 import { formatDate } from "../date";
+import { cn } from "app/lib/utils";
+import TableOfContent from "app/components/toc";
 export async function generateStaticParams() {
-	let posts = getBlogPosts();
+  let posts = getBlogPosts();
 
-	return posts.map((post) => ({
-		slug: post.slug,
-	}));
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
 }
 
 export function generateMetadata({ params }) {
-	let post = getBlogPosts().find((post) => post.slug === params.slug);
-	if (!post) return;
-	let {
-		title,
-		publishedAt: publishedTime,
-		summary: description,
-		image,
-	} = post.metadata;
-	let ogImage = image
-		? image
-		: `${baseUrl}/og?title=${encodeURIComponent(title)}`;
+  let post = getBlogPosts().find((post) => post.slug === params.slug);
+  if (!post) return;
+  let {
+    title,
+    publishedAt: publishedTime,
+    summary: description,
+    image,
+  } = post.metadata;
+  let ogImage = image
+    ? image
+    : `${baseUrl}/og?title=${encodeURIComponent(title)}`;
 
-	return {
-		title,
-		description,
-		openGraph: {
-			title,
-			description,
-			type: "article",
-			publishedTime,
-			url: `${baseUrl}/blog/${post.slug}`,
-			images: [
-				{
-					url: ogImage,
-				},
-			],
-		},
-		twitter: {
-			card: "summary_large_image",
-			title,
-			description,
-			images: [ogImage],
-		},
-	};
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "article",
+      publishedTime,
+      url: `${baseUrl}/blog/${post.slug}`,
+      images: [
+        {
+          url: ogImage,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [ogImage],
+    },
+  };
 }
 
 export default function Blog({ params }) {
-	let post = getBlogPosts().find((post) => post.slug === params.slug);
+  let post = getBlogPosts().find((post) => post.slug === params.slug);
+  if (!post) {
+    notFound();
+  }
+  const toc = parseToc(post.content);
 
-	if (!post) {
-		notFound();
-	}
-
-	return (
-		<section>
-			<script
-				type="application/ld+json"
-				suppressHydrationWarning
-				dangerouslySetInnerHTML={{
-					__html: JSON.stringify({
-						"@context": "https://schema.org",
-						"@type": "BlogPosting",
-						headline: post.metadata.title,
-						datePublished: post.metadata.publishedAt,
-						dateModified: post.metadata.publishedAt,
-						description: post.metadata.summary,
-						image: post.metadata.image
-							? `${baseUrl}${post.metadata.image}`
-							: `/og?title=${encodeURIComponent(post.metadata.title)}`,
-						url: `${baseUrl}/blog/${post.slug}`,
-						author: {
-							"@type": "Person",
-							name: "My Portfolio",
-						},
-					}),
-				}}
-			/>
-			<h1 className="title font-semibold text-2xl tracking-tighter">
-				{post.metadata.title}
-			</h1>
-			<div className="flex justify-between items-center mt-2 mb-8 text-sm">
-				<p className="text-sm text-neutral-600 dark:text-neutral-400">
-					{formatDate(post.metadata.publishedAt)}
-				</p>
-			</div>
-			<article className="prose">
-				<CustomMDX source={post.content} />
-			</article>
-		</section>
-	);
+  return (
+    <>
+      <aside
+        className={cn(
+          "fixed top-page flex max-w-[220px] -translate-x-[230px] translate-y-[140px] flex-col",
+          "transition-opacity lg:opacity-0",
+          "md:relative md:top-0 md:-ml-2 md:mb-7 md:translate-x-0 md:opacity-100",
+        )}
+      >
+        <TableOfContent
+          toc={toc}
+          data-animate
+          className="px-2 text-sm md:hidden"
+        />
+      </aside>
+      <section>
+        <script
+          type="application/ld+json"
+          suppressHydrationWarning
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "BlogPosting",
+              headline: post.metadata.title,
+              datePublished: post.metadata.publishedAt,
+              dateModified: post.metadata.publishedAt,
+              description: post.metadata.summary,
+              image: post.metadata.image
+                ? `${baseUrl}${post.metadata.image}`
+                : `/og?title=${encodeURIComponent(post.metadata.title)}`,
+              url: `${baseUrl}/blog/${post.slug}`,
+              author: {
+                "@type": "Person",
+                name: "Inu Jung",
+              },
+            }),
+          }}
+        />
+        <h1 className="title font-semibold text-2xl tracking-tighter">
+          {post.metadata.title}
+        </h1>
+        <div className="flex justify-between items-center mt-2 mb-8 text-sm">
+          <p className="text-sm text-neutral-600 dark:text-neutral-400">
+            {formatDate(post.metadata.publishedAt)}
+          </p>
+        </div>
+        <article className="prose">
+          <CustomMDX source={post.content} />
+        </article>
+      </section>
+    </>
+  );
 }
--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -5,106 +5,146 @@ import React from "react";
 import { highlight } from "sugar-high";
 
 function Table({ data }) {
-	let headers = data.headers.map((header, index) => (
-		<th key={index}>{header}</th>
-	));
-	let rows = data.rows.map((row, index) => (
-		<tr key={index}>
-			{row.map((cell, cellIndex) => (
-				<td key={cellIndex}>{cell}</td>
-			))}
-		</tr>
-	));
+  let headers = data.headers.map((header, index) => (
+    <th key={index}>{header}</th>
+  ));
+  let rows = data.rows.map((row, index) => (
+    <tr key={index}>
+      {row.map((cell, cellIndex) => (
+        <td key={cellIndex}>{cell}</td>
+      ))}
+    </tr>
+  ));
 
-	return (
-		<table>
-			<thead>
-				<tr>{headers}</tr>
-			</thead>
-			<tbody>{rows}</tbody>
-		</table>
-	);
+  return (
+    <table>
+      <thead>
+        <tr>{headers}</tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
 }
 
 function CustomLink(props) {
-	let href = props.href;
+  let href = props.href;
 
-	if (href.startsWith("/")) {
-		return (
-			<Link href={href} {...props}>
-				{props.children}
-			</Link>
-		);
-	}
+  if (href.startsWith("/")) {
+    return (
+      <Link href={href} {...props}>
+        {props.children}
+      </Link>
+    );
+  }
 
-	if (href.startsWith("#")) {
-		return <a {...props} />;
-	}
+  if (href.startsWith("#")) {
+    return <a {...props} />;
+  }
 
-	return <a target="_blank" rel="noopener noreferrer" {...props} />;
+  return <a target="_blank" rel="noopener noreferrer" {...props} />;
 }
 
 function RoundedImage(props) {
-	return <Image alt={props.alt} className="rounded-lg" {...props} />;
+  return <Image alt={props.alt} className="rounded-lg" {...props} />;
 }
 
 function Code({ children, ...props }) {
-	if (typeof children === "undefined") return;
-	let codeHTML = highlight(children);
-	return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
+  if (typeof children === "undefined") return;
+  let codeHTML = highlight(children);
+  return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
 }
 
 function slugify(str) {
-	return str
-		.toString()
-		.toLowerCase()
-		.trim() // Remove whitespace from both ends of a string
-		.replace(/\s+/g, "-") // Replace spaces with -
-		.replace(/&/g, "-and-") // Replace & with 'and'
-		.replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
-		.replace(/\-\-+/g, "-"); // Replace multiple - with single -
+  return str
+    .toString()
+    .toLowerCase()
+    .trim() // Remove whitespace from both ends of a string
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
+    .replace(/\-\-+/g, "-"); // Replace multiple - with single -
 }
 
 function createHeading(level) {
-	const Heading = ({ children }) => {
-		let slug = slugify(children);
-		return React.createElement(
-			`h${level}`,
-			{ id: slug },
-			[
-				React.createElement("a", {
-					href: `#${slug}`,
-					key: `link-${slug}`,
-					className: "anchor",
-				}),
-			],
-			children,
-		);
-	};
+  const Heading = ({ children }) => {
+    let slug = slugify(children);
+    return React.createElement(
+      `h${level}`,
+      { id: slug },
+      [
+        React.createElement("a", {
+          href: `#${slug}`,
+          key: `link-${slug}`,
+          className: "anchor",
+        }),
+      ],
+      children,
+    );
+  };
 
-	Heading.displayName = `Heading${level}`;
+  Heading.displayName = `Heading${level}`;
 
-	return Heading;
+  return Heading;
 }
 
 let components = {
-	h1: createHeading(1),
-	h2: createHeading(2),
-	h3: createHeading(3),
-	h4: createHeading(4),
-	h5: createHeading(5),
-	h6: createHeading(6),
-	Image: RoundedImage,
-	a: CustomLink,
-	code: Code,
-	Table,
+  h1: createHeading(1),
+  h2: createHeading(2),
+  h3: createHeading(3),
+  h4: createHeading(4),
+  h5: createHeading(5),
+  h6: createHeading(6),
+  Image: RoundedImage,
+  a: CustomLink,
+  code: Code,
+  Table,
+};
+export type TOCSection = TOCSubSection & {
+  subSections: TOCSubSection[];
+};
+
+export type TOCSubSection = {
+  slug: string;
+  text: string;
+};
+export const parseToc = (source: string) => {
+  return source
+    .split("\n")
+    .filter((line) => line.match(/(^#{1,3})\s/))
+    .reduce<TOCSection[]>((ac, rawHeading) => {
+      const nac = [...ac];
+      const removeMdx = rawHeading
+        .replace(/^##*\s/, "")
+        .replace(/[\*,\~]{2,}/g, "")
+        .replace(/(?<=\])\((.*?)\)/g, "")
+        .replace(/(?<!\S)((http)(s?):\/\/|www\.).+?(?=\s)/g, "");
+
+      const section = {
+        slug: removeMdx
+          .trim()
+          .toLowerCase()
+          .replace(/[^a-z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣 -]/g, "")
+          .replace(/\s/g, "-"),
+        text: removeMdx,
+      };
+
+      const isSubTitle = rawHeading.split("#").length - 1 === 3;
+
+      if (ac.length && isSubTitle) {
+        nac.at(-1)?.subSections.push(section);
+      } else {
+        nac.push({ ...section, subSections: [] });
+      }
+
+      return nac;
+    }, []);
 };
 
 export function CustomMDX(props) {
-	return (
-		<MDXRemote
-			{...props}
-			components={{ ...components, ...(props.components || {}) }}
-		/>
-	);
+  return (
+    <MDXRemote
+      {...props}
+      components={{ ...components, ...(props.components || {}) }}
+    />
+  );
 }
