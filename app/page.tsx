d3bbb4c2a70d8670511f46328306fe3fcf0dc66a
inujung <inujung@momenti.tv>
2024-04-11 16:03:40 +0900

    edit: parellel route

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
-	return (
-		<section>
-			<h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-				My Portfolio
-			</h1>
-			<p className="mb-4">
-				{`I'm a junior frontend developer, who is widely interested in 500 shades of programming. `}
-			</p>
-			<div className="my-8">
-				<BlogPosts />
-			</div>
-		</section>
-	);
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        My Portfolio
+      </h1>
+      <p className="mb-4">
+        {`I'm a junior frontend developer, who is widely interested in 500 shades of programming. `}
+      </p>
+      <div className="my-8">
+        <BlogPosts />
+      </div>
+    </section>
+  );
 }
