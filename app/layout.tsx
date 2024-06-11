cd0393f41e8b4dd40b365190ef6bfa0d96f08bfe
inujung <inujung@momenti.tv>
2024-06-11 20:44:47 +0900

    feat: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,64 +10,64 @@ import { cn } from "./lib/utils";
 import { baseUrl } from "./sitemap";
 
 export const metadata: Metadata = {
-	metadataBase: new URL(baseUrl),
-	title: {
-		default: "Inu's blog",
-		template: "%s | Inu's blog",
-	},
-	description: "Diary of Inu, a developer",
-	openGraph: {
-		title: "Inu's blog",
-		description: "Diary of Inu, a developer",
-		url: baseUrl,
-		siteName: "Inuslog",
-		locale: "en_US",
-		type: "website",
-	},
-	robots: {
-		index: true,
-		follow: true,
-		googleBot: {
-			index: true,
-			follow: true,
-			"max-video-preview": -1,
-			"max-image-preview": "large",
-			"max-snippet": -1,
-		},
-	},
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: "Inu's blog",
+    template: "%s | Inu's blog",
+  },
+  description: "Diary of Inu, a developer",
+  openGraph: {
+    title: "Inu's blog",
+    description: "Diary of Inu, a developer",
+    url: baseUrl,
+    siteName: "Inuslog",
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html
-			lang="en"
-			className={cx(
-				"text-black bg-white dark:text-white dark:bg-black",
-				GeistSans.variable,
-				GeistMono.variable,
-			)}
-		>
-			<body className="antialiased mx-4 mt-8 lg:mx-auto">
-				<main>
-					<aside
-						id="toc"
-						className={cn(
-							"fixed top-[128px] flex max-w-[220px] -translate-x-[230px] translate-y-[140px] flex-col mr-4",
-						)}
-					></aside>
-					<Navbar />
-					{children}
-					<Footer />
-					<Analytics />
-					<SpeedInsights />
-				</main>
-			</body>
-		</html>
-	);
+  return (
+    <html
+      lang="en"
+      className={cx(
+        "text-black bg-white dark:text-white dark:bg-black",
+        GeistSans.variable,
+        GeistMono.variable,
+      )}
+    >
+      <body className="antialiased mx-4 mt-8 lg:mx-auto">
+        <main>
+          <aside
+            id="toc"
+            className={cn(
+              "fixed top-[128px] flex max-w-[220px] -translate-x-[230px] translate-y-[140px] flex-col mr-4",
+            )}
+          ></aside>
+          <Navbar />
+          {children}
+          <Footer />
+          <Analytics />
+          <SpeedInsights />
+        </main>
+      </body>
+    </html>
+  );
 }
